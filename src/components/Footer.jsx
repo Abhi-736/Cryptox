ee7d4e70177986050fbd65fb6d8ab0399ed36a84
Harry <abhishekchauhan664@gmail.com>
2023-01-16 18:03:35 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,38 @@
+import { Avatar, Box, Stack, Text, VStack } from "@chakra-ui/react";
+import React from "react";
+
+const avatarSrc = "https://i.ibb.co/68M6Xnh/final.png"
+
+const Footer = () => {
+  return (
+    <Box
+      bgColor={"blackAlpha.900"}
+      color={"whiteAlpha.700"}
+      minH={"48"}
+      px={"16"}
+      py={["16", "8"]}
+    >
+      <Stack direction={["column", "row"]} h={"full"} alignItems={"center"}>
+        <VStack w={"full"} alignItems={["center", "flex-start"]}>
+          <Text fontWeight={"bold"}>About Us</Text>
+          <Text
+            fontSize={"sm"}
+            letterSpacing={"widest"}
+            textAlign={["center", "left"]}
+          >
+            The best crypto app that updates you about the status of various crypto currencies.
+          </Text>
+        </VStack>
+
+        <VStack><a href="https://abhi736.000webhostapp.com/#Home">
+          <Avatar boxSize={"28"} mt={["4", "0"]} src={avatarSrc} />
+          </a>
+          <Text >Designed by</Text>
+        
+          </VStack>
+      </Stack>
+    </Box>
+  );
+};
+
+export default Footer;
